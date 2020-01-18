dd2210d4d5cc33ee34a24bca2b86896cdcbc60b0
Clément Dubreuil <clement.dub60@gmail.com>
2020-01-18 19:15:12 +0100

    check file existance

--- src/main.php
+++ src/main.php
@@ -8,9 +8,13 @@ if ($argc > 2) {
     echo "Usage: plox [script]\n";
     exit(64);
 } else if ($argc === 2) {
-    $lox = new Lox();
-    $hadError = $lox->run(file_get_contents($argv[1]));
-    if ($hadError) exit(65);
+    if (file_exists($argv[1])) {
+        $lox = new Lox();
+        $hadError = $lox->run(file_get_contents($argv[1]));
+        if ($hadError) exit(65);
+    } else {
+        fwrite(STDERR, 'The file \''.$argv[1]."' does not exist.\n");
+    }
 }
 else {
     $lox = new Lox();
